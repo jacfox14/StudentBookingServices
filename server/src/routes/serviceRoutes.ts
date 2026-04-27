5af4d609496011404487cedff8bd649e64eebf4c
Jacob Fox <jacfox14@gmail.com>
2026-04-26 21:28:21 -0700

    fixed minor bug

--- server/src/routes/serviceRoutes.ts
+++ server/src/routes/serviceRoutes.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { requireAuth } from "../middleware/requireAuth.js";
 import { serviceController } from "../controllers/serviceController.js";
 
 const router = Router();
@@ -6,6 +7,6 @@ const router = Router();
 router.get("/service-categories", serviceController.categories);
 router.get("/services", serviceController.list);
 router.get("/services/:id", serviceController.get);
-router.get("/services/:id/availability", serviceController.availability);
+router.get("/services/:id/availability", requireAuth, serviceController.availability);
 
 export default router;
